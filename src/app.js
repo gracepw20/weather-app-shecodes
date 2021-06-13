f58e9657df944bf8e7fdeddf275884ceaad3a71f
gracepw20 <gracepw20@googlemail.com>
2021-06-13 13:23:56 +0100

    updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+function formatDate(timestamp) {
+let date = new Date(timestamp)
+let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`
+}
+let days = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"]
+let day = days[date.getDay()];
+return `${day}, ${hours}:${minutes}`
+}
+
+
+
 function displayTemperature(response) {
 let temperatureElement = document.querySelector("#current-temp")
 temperatureElement.innerHTML = Math.round(response.data.main.temp)
@@ -7,8 +24,10 @@ let descriptionElement = document.querySelector("#current-desc")
 descriptionElement.innerHTML = response.data.weather[0].description
 let humidity = document.querySelector("#humidity")
 humidity.innerHTML = response.data.main.humidity
-let wind = document.querySelector("#wind")
-wind.innerHTML = Math.round(response.data.main.wind.speed)
+let windElement = document.querySelector("#wind")
+windElement.innerHTML = Math.round(response.data.wind.speed)
+let dateElement = document.querySelector("#date")
+dateElement.innerHTML = formatDate(response.data.dt * 1000)
 }
 
 
