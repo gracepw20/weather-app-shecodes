f3552d3803cd5f8f4de96c0a823af393cf0c8d86
gracepw20 <gracepw20@googlemail.com>
2021-06-13 15:25:19 +0100

    added JS to form

--- src/app.js
+++ src/app.js
@@ -33,9 +33,19 @@ iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data
 iconElement.setAttribute("alt", response.data.weather[0].description)
 }
 
-
+function search(city) {
 let apiKey = "f94c6d984db626b788cf81becaec5145"
-let city = "London"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-
 axios.get(apiUrl).then(displayTemperature)
+}
+
+function handleSubmit(event) {
+    event.preventDefault()
+    let cityInputElement = document.querySelector("#city-input")
+    search(cityInputElement.value)
+}
+
+let form = document.querySelector("#search-form")
+form.addEventListener("submit", handleSubmit)
+
+search("London")
